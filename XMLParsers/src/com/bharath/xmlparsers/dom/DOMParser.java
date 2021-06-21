fd5963ad201e54399f31eb45e67ed162ed4e1b0e
kuanyilee85 <kuanyilee85@gmail.com>
2021-06-21 15:10:43 -0400

    80. Read the Elements Hierarchically

--- XMLParsers/src/com/bharath/xmlparsers/dom/DOMParser.java
+++ XMLParsers/src/com/bharath/xmlparsers/dom/DOMParser.java
@@ -1,5 +1,6 @@
 package com.bharath.xmlparsers.dom;
 
+import com.bharath.xmlparsers.dto.Address;
 import com.bharath.xmlparsers.dto.DriversLicense;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -35,9 +36,42 @@ public class DOMParser {
         Element documentElement = document.getDocumentElement();
         license.setStatus(documentElement.getAttribute("status"));
 
+        NodeList addressList = document.getElementsByTagName("Address");
+        Node addressNode = addressList.item(0);
+        NodeList addressChildNodes = addressNode.getChildNodes();
+        Address address = new Address();
+
+        for (int i = 0; i < addressChildNodes.getLength(); i++) {
+            Node item = addressChildNodes.item(i);
+
+            if (item instanceof Element) {
+                switch (item.getNodeName()){
+                    case "street":
+                        address.setStreet(item.getTextContent());
+                        break;
+                    case "city":
+                        address.setCity(item.getTextContent());
+                        break;
+                    case "state":
+                        address.setState(item.getTextContent());
+                        break;
+                    case "country":
+                        address.setCountry(item.getTextContent());
+                        break;
+                    case "zipcode":
+                        address.setZipcode(item.getTextContent());
+                        break;
+
+                }
+            }
+        }
+
+        license.setAddress(address);
+
         System.out.println(license.getNumber());
         System.out.println(license.getFirstName());
         System.out.println(license.getLastName());
         System.out.println(license.getStatus());
+        System.out.println(license.getAddress().getCity());
     }
 }
