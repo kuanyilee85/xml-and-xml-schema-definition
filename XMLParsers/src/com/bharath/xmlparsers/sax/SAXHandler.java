f9a8deac4e7fe5985524687a83e46f4a5fb60d20
kuanyilee85 <kuanyilee85@gmail.com>
2021-06-23 09:03:28 -0400

    84. Implement the Handler Methods

--- XMLParsers/src/com/bharath/xmlparsers/sax/SAXHandler.java
+++ XMLParsers/src/com/bharath/xmlparsers/sax/SAXHandler.java
@@ -1,30 +1,46 @@
 package com.bharath.xmlparsers.sax;
 
+import com.bharath.xmlparsers.dto.DriversLicense;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
 public class SAXHandler extends DefaultHandler {
 
+    private DriversLicense driversLicense;
+    private String content;
+
     @Override
     public void startElement (String uri, String localName,
                               String qName, Attributes attributes)
             throws SAXException
     {
-
+        if(qName.equals("DriversLicense")){
+            driversLicense = new DriversLicense();
+        }
     }
 
     @Override
     public void endElement (String uri, String localName, String qName)
             throws SAXException
     {
-
+        switch (qName){
+            case "Number":
+                driversLicense.setNumber(Long.parseLong(content));
+                break;
+            case "FirstName":
+                driversLicense.setFirstName(content);
+                break;
+            case "LastName":
+                driversLicense.setLastName(content);
+                break;
+        }
     }
 
     @Override
     public void characters (char ch[], int start, int length)
             throws SAXException
     {
-
+        content = String.copyValueOf(ch, start, length).trim();
     }
 }
